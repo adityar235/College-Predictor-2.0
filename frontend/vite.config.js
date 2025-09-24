8a4282a51a3ce488c9085014b9e347daac632155
aditya <aditya.rouss@gmail.com>
2025-09-24 19:40:41 +0530

    port problem

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        // target: 'http://localhost:5000',
+        target: 'https://college-predictor-2-0.onrender.com',
         changeOrigin: true
       }
     }
