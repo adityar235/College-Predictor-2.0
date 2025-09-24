e70f0cb257d1be4c75c04119ddd836f4ae564812
aditya <aditya.rouss@gmail.com>
2025-09-24 19:46:09 +0530

    this is updated end point

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,8 +16,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://localhost:5000',
-        target: 'https://college-predictor-2-0.onrender.com',
+        target: 'http://localhost:5000',
+        // target: 'https://college-predictor-2-0.onrender.com',
         changeOrigin: true
       }
     }
