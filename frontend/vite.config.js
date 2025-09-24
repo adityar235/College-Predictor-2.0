c6ae57ad3df9ee32b013c1ece6902145c12572f6
aditya <aditya.rouss@gmail.com>
2025-09-24 23:56:24 +0530

    s

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://college-predictor-2-0-ka8d.vercel.app/',
         changeOrigin: true
       }
     }
