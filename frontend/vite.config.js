fe8e17b1f02a2489d374f1d412c41c8ca760b3c6
aditya <168897164+adityar235@users.noreply.github.com>
2025-09-26 00:39:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,28 +1,28 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://college-predictor-2-0-cp21.vercel.app/',
-        changeOrigin: true
-      }
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://college-predictor-2-0-cp21.vercel.app/',
+//         changeOrigin: true
+//       }
+//     }
+//   },
+//   build: {
+//     outDir: 'dist',
+//     sourcemap: false
+//   }
+// })
